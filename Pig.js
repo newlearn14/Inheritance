ce105aa6a28c3236db686be22a00f403c5294f9a
newlearn14 <80698871+newlearn14@users.noreply.github.com>
2021-04-12 18:54:53 +0530

    inherit

---
+++ Pig.js
@@ -0,0 +1,10 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+      super(x,y,40,40)
+      this.image=loadImage("sprites/enemy.png")
+    }
+    display(){
+      super.display();
+    }
+  }
+  
